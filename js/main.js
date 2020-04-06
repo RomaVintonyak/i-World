30fea72cd91107045288c8f58836375197cb33e4
RomaVintonyak <19roman23@gmail.com>
2020-04-06 12:48:40 +0300

    create mobile menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 jQuery(document).ready(function(){
   "use script";
-  /*burger button script */
+  /*burger button script & open mobile menu*/
   var burgerBTn = $("#burgerBtn");
+  var menu = $(".mobile__row");
   burgerBTn.on("click", function(event){
     event.preventDefault();
     $(this).find("span").toggleClass("open");
+    menu.toggleClass("mobile__row--open");
+
   });
 });
